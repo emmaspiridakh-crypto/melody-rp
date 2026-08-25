92c1e94af90d0efe8cfabb4b02f8ec7b5d7379eb
emma sp <emmaspiridakh@gmail.com>
2026-08-26 01:12:35 +0300

    Update tickets.py

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -48,6 +48,12 @@ def _ticket_types() -> dict:
             "category_id": config.CAT_TICKET_PARTNERSHIP_ID,
             "view_roles": [config.OWNERSHIP_ROLE_ID, config.SERVER_ORG_ID],
         },
+         "report": { 
+            "label": "Report",
+            "emoji": emoji("tickets", "report"},
+            "category_id": config.CAT_TICKET_REPORT_ID,
+            "view_roles": [config.OWNERSHIP_ROLE_ID, config.MANAGER_ROLE_ID],
+         },
         "report_staff": {
             "label": "Report Staff",
             "emoji": emoji("tickets", "report_staff"),
